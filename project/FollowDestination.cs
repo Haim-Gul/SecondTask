06c08efce2c6aee4b6316afc27f6de9be9d842d6
Haim-Gul <65022215+Haim-Gul@users.noreply.github.com>
2020-06-07 18:03:44 +0500

    Add files via upload

---
+++ project/FollowDestination.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowDestination : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private UnityEngine.AI.NavMeshAgent agent = null;
+    public Transform destination = null;
+    [SerializeField] float turnspeed = 45.0f;
+    [SerializeField] float speed = 10f;
+    private float horizontal;
+    private float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(destination.position);
+        //  horizontal = Input.GetAxis("Horizontal");
+        //vertical = Input.GetAxis("Vertical");
+
+        //  transform.Translate(Vector3.forward * speed * Time.deltaTime * vertical);
+        //transform.Rotate(Vector3.up, turnspeed * horizontal * Time.deltaTime);
+
+    }
+
+}
---
+++ project/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] float turnspeed = 45.0f;
+    [SerializeField] float speed = 20.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //  if (Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput);
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+
+        //}       
+    }
+}
